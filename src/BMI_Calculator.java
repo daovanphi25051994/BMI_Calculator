48580a5e5633bedaa0b9de112cd0ef71e5e0edd0
phi <daovanphi25051994@gmail.com>
2020-03-15 10:23:56 +0700

    bmi calculator

---
+++ src/BMI_Calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BMI_Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.printf("Your weight? (in kilogram): ");
+        double weight = scanner.nextDouble();
+        System.out.printf("Your height? (in meter): ");
+        double height = scanner.nextDouble();
+        double bmi = weight / Math.pow(height, 2);
+        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
+        if (bmi < 18) {
+            System.out.printf("%-20.2f%s", bmi, "Underweight");
+        } else if (bmi < 25) {
+            System.out.printf("%-20.2f%s", bmi, "Normal");
+        } else if (bmi < 30) {
+            System.out.printf("%-20.2f%s", bmi, "Overweight");
+        } else {
+            System.out.printf("%-20.2f%s", bmi, "Obese");
+        }
+    }
+}
